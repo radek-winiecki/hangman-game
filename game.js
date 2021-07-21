3673f6e1cf5a75a9212a69ef306cfc7fe503ae8e
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-22 01:07:57 +0200

    Replace the letters of the password with dashes

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 let password = "Practise makes perfect";
 password = password.toUpperCase();
 
+let length = password.length;
+let password1 = "";
+
+for (i = 0; i < length; i++) {
+    if (password[i] === " ") {
+        password1 = password1 + " ";
+    } else {
+        password1 = password1 + "-";
+    }
+}
+
 function write_password() {
-    document.getElementById("board").innerHTML = password;
+    document.getElementById("board").innerHTML = password1;
 }
 
 window.onload = write_password;
