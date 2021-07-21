8657ca29388a42939ded8559fc453311c9a34b29
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-22 01:33:04 +0200

    Create a function that displays array of 35 chars

--- game.js
+++ game.js
@@ -16,4 +16,18 @@ function write_password() {
     document.getElementById("board").innerHTML = password1;
 }
 
-window.onload = write_password;
+window.onload = start;
+
+function start() {
+    let div_content = "";
+    for (i = 0; i <= 34; i++) {
+        div_content = div_content + '<div class="letter">A</div>';
+        if ((i + 1) % 7 === 0) {
+            div_content = div_content + '<div style="clear:both;"></div>';
+        }
+    }
+
+    document.getElementById("alphabet").innerHTML = div_content;
+
+    write_password();
+}
